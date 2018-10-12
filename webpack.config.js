125cdf7b77d1232fcdd40244a334956326d9ffdc
Yoav Vainrich <yoavain@gmail.com>
2018-10-12 23:53:52 +0300

    Let webpack select external by itself

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,7 @@
 const webpack = require('webpack');
 const path = require('path');
-const fs = require('fs');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-var nodeModules = {};
-fs.readdirSync('node_modules')
-.filter(function(x) {
-	return ['.bin'].indexOf(x) === -1;
-})
-.forEach(function(mod) {
-	nodeModules[mod] = 'commonjs ' + mod;
-});
-  
 module.exports = {
 	entry: './compiled/index.js',
 	target: 'node',
@@ -19,7 +9,6 @@ module.exports = {
 		path: path.join(__dirname, 'built'),
 		filename: 'index.js'
 	},
-	externals: nodeModules,
 	plugins: [
 		new webpack.IgnorePlugin(/\.(css|less)$/),
 		new CopyWebpackPlugin([
