4de2dd1fd033993f463ee66d90cc0166fc0d2dc8
Yoav Vainrich <yoavain@gmail.com>
2018-10-19 19:03:23 +0300

    * Parse arguments by myself and not with yargs (since -- is not working) * Fix launcher script to handle more arguments

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    transform: {
+        "^.+\\.ts$": "ts-jest"
+    },
+    testRegex: "test\/.*.test.ts$",
+    moduleFileExtensions: [
+        "ts",
+        "js",
+        "json",
+        "node"
+    ],
+    verbose: true
+};
