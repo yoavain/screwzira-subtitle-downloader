19ee25875255d0141e9a4f14bde051720a1ebd83
Yoav Vainrich <yoavain@gmail.com>
2021-01-22 21:18:51 +0200

    update launcher to be based on create-windowless-app v5.x

--- launcher/launcherCompiler.ts
+++ launcher/launcherCompiler.ts
@@ -1,39 +1,30 @@
 import * as fs from "fs-extra";
 import * as path from "path";
 import spawn from "cross-spawn";
-import { ChildProcess } from "child_process";
+import type { SpawnSyncReturns } from "child_process";
 
-export function checkCscInPath(exit?: boolean): Promise<boolean> {
-    // Check for csc.exe in %PATH%
-    const promises = process.env.path.split(";").map(p => fs.pathExists(path.resolve(p, "csc.exe")));
-    return Promise.all(promises)
-        .then(results => {
-            return results.find(result => !!result);
-        })
-        .then(result => {
-            if (exit && !result) {
-                console.error(`You need "csc.exe" (C# compiler) in your path in order to compile the launcher.exe.`);
-                process.exit(1);
-            }
-            else {
-                return !!result
-            }
-        })
-}
+const COMPILER: string = "msbuild.exe";
 
-export function compileLauncher(sourceLocation: string, outputLocation: string, iconLocation: string): Promise<void> {
-    return new Promise(((resolve, reject) => {
-        const command: string = 'csc.exe';
-        const args: string[] = ["/t:winexe", `/out:${outputLocation}`, `/win32icon:${iconLocation}`, `${sourceLocation}`];
-        const child: ChildProcess = spawn(command, args, { stdio: 'inherit' });
-        child.on('close', code => {
-            if (code !== 0) {
-                reject({
-                    command: `${command} ${args.join(' ')}`,
-                });
-                return;
-            }
-            resolve();
-        });
-    }))
-}
+export const checkMsbuildInPath = async (exit?: boolean): Promise<boolean> => {
+    // Check for compiler in %PATH%
+    const promises = process.env.path.split(";").map((p) => fs.pathExists(path.resolve(p, COMPILER)));
+    const results: boolean[] = await Promise.all(promises);
+    const compilerFound: boolean = await results.find((result) => !!result);
+
+    if (exit && !compilerFound) {
+        console.error(`You need "${COMPILER}" in your %PATH% in order to compile the launcher executable.`);
+        process.exit(1);
+    }
+    else {
+        return compilerFound;
+    }
+};
+
+export const compileLauncher = async (): Promise<void> => {
+    const args: string[] = ["./launcher/launcher.csproj"];
+
+    const spawnResult: SpawnSyncReturns<Buffer> = spawn.sync(COMPILER, args, { stdio: "inherit" });
+    if (spawnResult.status !== 0) {
+        return Promise.reject({ command: `${COMPILER} ${args.join(" ")}` });
+    }
+};
